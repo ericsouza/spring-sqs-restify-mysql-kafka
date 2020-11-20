26e31209b112cdb0093c57935f0eaa5bf24cc2f1
Eric Souza <ericsouza0801@gmail.com>
2020-11-19 22:50:04 -0300

    first commit

---
+++ src/main/java/ericsouza/github/com/springsqsrestifymysqlkafka/SpringSqsRestifyMysqlKafkaApplication.java
@@ -0,0 +1,13 @@
+package ericsouza.github.com.springsqsrestifymysqlkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSqsRestifyMysqlKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSqsRestifyMysqlKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ericsouza/github/com/springsqsrestifymysqlkafka/SpringSqsRestifyMysqlKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ericsouza.github.com.springsqsrestifymysqlkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsRestifyMysqlKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
